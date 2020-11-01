272db4efdbd89311972e5ff687b862e2facc5192
Mynul <Mynul@DESKTOP-OQKABRM>
2020-11-01 18:24:02 -0500

    I update the java and TestNG Version

--- src/Automation.java
+++ src/Automation.java
@@ -2,7 +2,8 @@
 public class Automation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Automation is great");
+		System.out.println("Automation is great");
 
 	}
 
